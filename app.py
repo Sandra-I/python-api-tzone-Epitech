bcca26944256a47ee263c19f030f68830e069f27
S-I <sandra.ibrahim@hotmail.fr>
2021-07-16 17:06:02 +0200

    The final one !

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-#app.py
 from flask import Flask, flash, request, redirect, url_for, render_template, jsonify
 import urllib.request
 import os
+import base64
+from PIL import Image
+import io
 from werkzeug.utils import secure_filename
 from imgToText import process_image
  
@@ -21,34 +23,23 @@ def allowed_file(filename):
  
 @app.route('/')
 def home():
-    return render_template('index.html')
-    # return 'Text'
+    return 'Well pinging!'
  
-@app.route('/', methods=['POST'])
-def upload_image():
-    if 'file' not in request.files:
-        # flash('No file part')
-        # return redirect(request.url)
-        return 'no file'
-    file = request.files['file']
-    if file.filename == '':
-        flash('No image selected for uploading')
-        return redirect(request.url)
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        #print('upload_image filename: ' + filename)
-        #flash('Image successfully uploaded and displayed below')
-        flash(process_image(os.path.join(app.config['UPLOAD_FOLDER'], filename)))
-        return render_template('index.html', filename=filename)
-    else:
-        flash('Allowed image types are - png, jpg, jpeg, gif')
-        return redirect(request.url)
- 
-@app.route('/display/<filename>')
-def display_image(filename):
-    #print('display_image filename: ' + filename)
-    return redirect(url_for('static', filename='uploads/' + filename), code=301)
- 
+@app.route('/upload', methods=['POST']) 
+def upload_base64_file(): 
+    """ 
+        Upload image with base64 format and get car make model and year 
+        response 
+    """
+    data = request.get_json()
+    data = data['img']    
+    image = base64.b64decode(data)       
+    imagePath = ('./static/uploads/test.png')
+    img = Image.open(io.BytesIO(image))
+    img.save(imagePath)
+    textToReturn = process_image(imagePath)
+    return jsonify({ 'text': textToReturn })
+    
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port = int(8080))
