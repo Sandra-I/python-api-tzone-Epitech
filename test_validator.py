def4091629a8810e0a14b67796e19ec40c3a6274
Reyane-R <reyane.a.remache@gmail.com>
2021-11-26 16:16:17 +0000

    Update test_validator.py

--- test_validator.py
+++ test_validator.py
@@ -4,10 +4,10 @@ from validator import translation_allowed, validate_json
 
 class testClass(unittest.TestCase):
     def test_translation_allowed(self):
-        jsonData = json.loads('{"lang": "ES", "r": 25, "marks": 72}')
+        jsonData = json.loads('{"language": "ES", "r": 25, "marks": 72}')
         result = translation_allowed(jsonData)
         self.assertEqual(result, True)
     def test_validate_json(self):
-        jsonData = json.loads('{"lang": "ES", "img": "gggg"}')
+        jsonData = json.loads('{"language": "ES", "img": "gggg"}')
         result = validate_json(jsonData)
         self.assertEqual(result, True)
