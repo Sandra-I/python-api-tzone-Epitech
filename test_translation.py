90db54af68d9671a865781b9fe88d53425cbabd8
Reyane-R <reyane.a.remache@gmail.com>
2021-11-28 10:06:51 +0000

    Delete test_translation.py

--- test_translation.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from translation import translation_text
-
-class testClass(unittest.TestCase):
-    def test_translation_text(self):
-        result = translation_text("name","ES")
-        target='nombre'
-        self.assertEqual(result.text, target)
